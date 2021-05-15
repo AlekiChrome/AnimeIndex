d7fb771a0d1b5a3f681e6bd7cc5996c48a8878a7
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-05-15 13:04:07 -0400

    Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import { useState, useEffect } from 'react';
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
+	const [ animeList, SetAnimeList] = useState([]);
+	const [topAnime,SetTopAnime] = useState([]);
+	const [search, setSearch] = useState("");
+
+
+
 	return (
 		<div className="App">
 			<Header />
+			<div className="content-wrap">
+
+				<Sidebar
+					topAnime={topAnime}/>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Sidebar = () => {
+    return (
+        <aside>
+            <nav>
+            <h3>Top Anime</h3>
+            <a href="#"
+               target="_blank"
+               rel="noreferrer">Demon Slayer</a>
+            </nav>
+        </aside>
+    )
+}
+
+export default Sidebar;
