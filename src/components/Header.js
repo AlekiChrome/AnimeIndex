76643b531ec587769b8c37ef456362e94992347f
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-05-15 12:42:24 -0400

    scss files, header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1><strong>Anime</strong>Index</h1>
+        </div>
+    )
+}
+
+export default Header;
