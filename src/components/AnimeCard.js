827ad512231130b3fee24c0b98d14f2fb79b0d1c
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-05-16 06:24:30 -0400

    url included onto card

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -3,9 +3,14 @@ import React from "react";
 const AnimeCard = ({anime}) => {
     return (
        <article className="anime-card">
-           <a href="">
+           <a
+            href={anime.url}
+            target="_blank"
+            rel="noreferrer">
                <figure>
-                   <img src={anime.image_url} alt="anime image"/>
+                   <img
+                    src={anime.image_url}
+                    alt="anime image"/>
                </figure>
                <h3>{ anime.title }</h3>
            </a>
